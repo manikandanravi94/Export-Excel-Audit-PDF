63c54bd69398bbb1e32b889f8181717575e2e6a1
manikandanravi94 <manikandanravi94@gmail.com>
2020-03-08 12:21:04 +0530

    Added initial project setup files and pushing

---
+++ src/main/java/com/audit/ExcelReaderApplication.java
@@ -0,0 +1,14 @@
+package com.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelReaderApplication.class, args);
+		System.out.println("started application");
+	}
+
+}
